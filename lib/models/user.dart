53864ebce4af558c502d9989dd049e27477903fc
davidjz99 <aangel.david@hotmail.com>
2025-09-04 11:46:15 -0600

    diseno login e implementacion de funcion basica
    se mejoro el diseno del login y se creo una funcion que se ejecuta al momento de hacer clic en el boton, de momento la funcion no hace nada aun pero esta lista para desarrollar logica ahi dentro

---
+++ lib/models/user.dart
@@ -0,0 +1,49 @@
+class User {
+  final int? userId;
+  final String? name;
+  final String? email;
+  final String? password;
+  final int? age;
+  final double? height;
+  final double? weight;
+
+  // Constructor, con todos los parametros opcionales
+  User({
+    this.userId,
+    this.name,
+    this.email,
+    this.password,
+    this.age,
+    this.height,
+    this.weight,
+  });
+
+  //se usa para crear una nueva instancia con valores actualizados
+  User copyWith({
+    int? userId,
+    String? name,
+    String? email,
+    String? password,
+    int? age,
+    double? height,
+    double? weight,
+  }) {
+    return User(
+      userId: userId ?? this.userId,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      password: password ?? this.password,
+      age: age ?? this.age,
+      height: height ?? this.height,
+      weight: weight ?? this.weight,
+    );
+  }
+
+  // Método opcional: crea un usuario para el login
+  User.forLogin({required this.email, required this.password})
+    : userId = null,
+      name = null,
+      age = null,
+      height = null,
+      weight = null;
+}
--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,7 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:health_app/models/user.dart';
 
-class LoginScreen extends StatelessWidget {
+//crear el StatefulWidget (este es dinamico)
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+//crear la clase de estado
+class _LoginScreenState extends State<LoginScreen> {
+  //controladores alamcenar el texto tecleado en los inputs de email y password
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  //declaramos las variables necesarias
+  String _message = '';
+
+  //se ejecuta al hacer clic en el boton, para realizar la accion del login
+  void _login() {
+    print('entra login');
+    final String email = _emailController.text;
+    final String password = _passwordController.text;
+
+    // Creamos una nueva instancia de la clase User
+    final User userLogin = User.forLogin(email: email, password: password);
+
+    print(userLogin.email);
+    print(userLogin.password);
+
+    // Llamamos a la funcion que se encarga de mostrar el texto
+    _showMessage();
+  }
+
+  //libera recursos cuando una pantalla ya no se utiliza, cuando el widget se destruye
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  //logica de una funcion
+  void _showMessage() {
+    setState(() {
+      if (_message != '') {
+        _message = '';
+        return;
+      }
+
+      _message = 'Sesión iniciada';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,14 +73,16 @@ class LoginScreen extends StatelessWidget {
             const SizedBox(
               height: 68.0,
             ), // Espacio entre el título y el primer input.
-            const TextField(
+            TextField(
+              controller: _emailController,
               decoration: InputDecoration(
                 labelText: 'Correo electrónico',
                 border: OutlineInputBorder(),
               ),
             ),
             const SizedBox(height: 16.0),
-            const TextField(
+            TextField(
+              controller: _passwordController,
               obscureText: true,
               decoration: InputDecoration(
                 labelText: 'Contraseña',
@@ -39,10 +92,20 @@ class LoginScreen extends StatelessWidget {
             const SizedBox(height: 32.0),
             ElevatedButton(
               onPressed: () {
-                // Aquí irá la lógica de inicio de sesión
-                print('presionaste enter');
-              },
+                _login();
+              }, //se ejecuta la funcion login al hacer clic en el boton
               child: const Text('Iniciar Sesión'),
+            ),
+            const SizedBox(height: 16.0),
+            Text(
+              //texto de "sesion iniciada"
+              _message,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.red,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         ),
